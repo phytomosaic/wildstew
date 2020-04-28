2b1e1882885a7a95e29970c61821e1e2922b0cb5
phytomosaic <smithr2@oregonstate.edu>
2020-04-27 18:17:52 -0700

    Include verbal trends and exceedances on one-glance summary page

--- R/app.R
+++ R/app.R
@@ -3,7 +3,7 @@
 #  Wilderness Stewardship Performance:
 #      Shiny app for automagically generated WSP reports
 #
-#    Rob Smith, phytomosaic@gmail.com, 16 Apr 2020
+#    Rob Smith, phytomosaic@gmail.com, 27 Apr 2020
 #
 ##      GNU General Public License, Version 3.0    ###################
 
